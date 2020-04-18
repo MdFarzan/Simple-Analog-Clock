824b1f80b0d9a856ae4413adb5e4a714d290b9cd
MdFarzan <mdfarzan776@gmail.com>
2020-04-18 11:48:55 +0530

    Second's arrow finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 //Write javaScript
+const hour = document.querySelector(".hours");
+const minute = document.querySelector(".minutes");
+const second = document.querySelector(".seconds");
+
+function setClock(){
+    let d = new Date();
+    let s = d.getSeconds();
+    //setting seconds
+    s = (360/60)*s;
+    
+    second.style.transform = "rotate("+s+"deg)";
+
+    //setting minutes
+    let m = d.getMinutes();
+    m = (360/60)*m;
+    console.log(m);
+
+    minute.style.transform = "rotate("+m+"deg)";
+
+
+
+    
+};
+
+setInterval(setClock,1000);
+
